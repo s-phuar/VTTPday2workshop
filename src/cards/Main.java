69057aa29d1925532f39510f7c689088a6cac281
s-phuar <coolbeansphuar123@gmail.com>
2024-10-03 19:45:54 +0800

    updated comments for clarity

--- src/cards/Main.java
+++ src/cards/Main.java
@@ -6,11 +6,13 @@ public class Main {
         //generate entirely new deck object, does not need parameter inputs
         Deck deck = new Deck();
 
+        //new Cards object takes the chosen card at position 51, taken from fresh deck
         Cards chosenCard = deck.get(51);
         System.out.printf("suite: %s, name: %s, value: %d\n", chosenCard.getSuite(), chosenCard.getRank(), chosenCard.getValue());
 
-        //shuffled deck
+        //shuffled the fresh deck instantiated at line 7
         deck.shuffleDeck();
+        //new Cards object takes the chosen card at position 51, taken from randomised deck
         Cards randomCard = deck.get(51);
         System.out.printf("suite: %s, name: %s, value: %d\n", randomCard.getSuite(), randomCard.getRank(), randomCard.getValue());
     
